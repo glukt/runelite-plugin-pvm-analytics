f7c2e41c57d97e800b502534fc464b68cc64e944
V3rTiGo <luketich_211@hotmail.com>
2025-09-13 00:16:41 -0800

    Adjustments - ExamplePlugin needs renamed

--- src/main/java/com/pvmanalytics/PvmAnalyticsPlugin.java
+++ src/main/java/com/pvmanalytics/PvmAnalyticsPlugin.java
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "PvM Analytics"
 )
-public class ExamplePlugin extends Plugin
+public class PvmAnalyticsPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private PvmAnalyticsConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    PvmAnalyticsConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(PvmAnalyticsConfig.class);
 	}
 }
--- src/test/java/com/pvmanalytics/PvmAnalyticsPluginTest.java
+++ src/test/java/com/pvmanalytics/PvmAnalyticsPluginTest.java
@@ -3,11 +3,11 @@ package com.pvmanalytics;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PvmAnalyticsPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PvmAnalyticsPlugin.class);
 		RuneLite.main(args);
 	}
 }
